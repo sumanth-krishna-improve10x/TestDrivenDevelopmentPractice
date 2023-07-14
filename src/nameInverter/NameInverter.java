44c7c2700b2195961108cf5f9afe23c45da1cbf2
Sumanth krishna M <sumanth.krishna@improve10x.com>
2023-07-14 15:42:20 +0530

    completed NameInverter.java

--- src/nameInverter/NameInverter.java
+++ src/nameInverter/NameInverter.java
@@ -1,7 +1,19 @@
 package nameInverter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NameInverter {
-    public String invertedName(String s) {
-        return null;
+    public String invertName(String s) {
+        String name = s.toLowerCase();
+        if (name.equals("")) {
+            return "";
+        } else if (!name.trim().contains(" ")) {
+            return name.trim();
+        } else {
+            String[] names = name.trim().split("\\s+");
+            return names[1] +", "+names[0];
+        }
     }
 }
+
--- src/nameInverter/NameInverterTest.java
+++ src/nameInverter/NameInverterTest.java
@@ -1,21 +1,50 @@
 package nameInverter;
 
-import org.testng.Assert;
+
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NameInverterTest {
 
     @Test
-    public void nothing(){
+    public void nothing() {
+    }
+
+    // input:"", output:""
+
+    @Test
+    public void givenEmpty_returnsEmpty() {
+        NameInverter nameInverter = new NameInverter();
+        String invertedName = nameInverter.invertName("");
+        assertEquals("", invertedName);
     }
 
     @Test
-    public void givenEmpty_ReturnsEmpty(){
+    public void givenName_returnName() {
         NameInverter nameInverter = new NameInverter();
-        String invertedName = nameInverter.invertedName(" ");
-        assertEquals("",invertedName);
+        String invertedName = nameInverter.invertName("Name");
+        assertEquals("name", invertedName);
+    }
+
+    @Test
+    public void givenSingleWordWithTrailingSpaces() {
+        NameInverter nameInverter = new NameInverter();
+        String invertedName = nameInverter.invertName("Name ");
+        assertEquals("name", invertedName);
+    }
+
+    @Test
+    public void givenSingleWordWithBeginingSpaces() {
+        NameInverter nameInverter = new NameInverter();
+        String invertedName = nameInverter.invertName(" Name ");
+        assertEquals("name", invertedName);
+    }
+
+    @Test
+    public void givenFirstLast_returnLastFirst() {
+        NameInverter nameInverter = new NameInverter();
+        String invertedName = nameInverter.invertName("First Last");
+        assertEquals("last, first", invertedName);
     }
 }
